4b3f03a5439e15ef5b8481adcaf850b6fb2187bf
runtime-error786 <mustafa782a@gmail.com>
2024-07-22 00:11:42 +0500

    tools adde

---
+++ tools.py
@@ -0,0 +1,7 @@
+
+from crewai_tools import SerperDevTool
+import os
+from crewai_tools import YoutubeChannelSearchTool
+
+
+youtube_tool = YoutubeChannelSearchTool(youtube_channel_handle='@easytuts4youcom')
